979c693b1ef4955d188530cbba96eb284049de42
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-04-05 19:55:54 -0400

    Set Up Project, MOngooDB,Schema,Save database, NodeJs Server

---
+++ routes/addUser.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var kittySchema = require('../model/kitySchema');
+
+var fluffy = new kittySchema({name:"kitty bd"})
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.send("hola from atom add");
+
+  fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+    console.log("Item Saved");
+});
+  // res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
